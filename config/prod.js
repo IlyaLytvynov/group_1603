c5089303cd590b38437ba4224e1e566df2274c4a
Ilya Lytvynov <ilyaillych@gmail.com>
2019-04-13 19:14:05 +0300

    Updated version of Webapck

--- config/prod.js
+++ config/prod.js
@@ -2,16 +2,7 @@ const webpack = require('webpack');
 
 
 const prod = (config, _path) => {
-    console.info("ENV: PROD");
-    // To enable uglify and decreasing bundle weight please uncommnet next lines
-
-    // const uglify = [new webpack.optimize.UglifyJsPlugin({
-    //     compress: {
-    //         warnings: false,
-    //         drop_console: false,
-    //     }
-    // })];
-    // config.plugins = config.plugins.concat(uglify);
+    config.mode = 'development';
     config.output.path =  _path;
 
     return config;
